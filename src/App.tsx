489bf73556276f66baf4ffec75c4240e413b0557
erinbedri <erinbedri@gmail.com>
2024-08-13 16:05:26 +0200

    Create bosic homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import HomePage from "./components/HomePage";
+
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<h1>Home Page</h1>} />
+                <Route path="/" element={<HomePage />} />
                 <Route path="/name-info" element={<h1>Tenant Name Info Page</h1>} />
                 <Route path="/email-info" element={<h1>Tenant Email Info Page</h1>} />
                 <Route path="/phone-info" element={<h1>Tenant Name Info Page</h1>} />
