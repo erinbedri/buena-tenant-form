0b6f4239c6541a300b0a9b2ca00566a3ee1e642f
erinbedri <erinbedri@gmail.com>
2024-08-16 13:56:12 +0200

    Reuse Form component in EmailPage

--- src/pages/EmailPage.tsx
+++ src/pages/EmailPage.tsx
@@ -1,44 +1,15 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import { useUserInfoContext } from "../contexts/UserInfoContext";
-import { BackButton, Button, TextInput } from "../components";
+import { Form } from "../components";
 import { PATHS } from "../utils/urls";
 
 const EmailPage: React.FC = () => {
-    const { userInfo, setUserInfo } = useUserInfoContext();
-    const [email, setEmail] = useState(userInfo.email);
-
-    const navigate = useNavigate();
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-
-        setUserInfo({ ...userInfo, email });
-
-        navigate(PATHS.PHONE);
-    };
-
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-6">
-            <h2 className="text-3xl font-semibold mb-6">Enter Your Email Address</h2>
-
-            <form onSubmit={handleSubmit} className="w-full max-w-xl">
-                <TextInput
-                    id="email"
-                    type="email"
-                    label="Email Address"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <div className="flex gap-5">
-                    <BackButton />
-
-                    {email && <Button type="submit">Next</Button>}
-                </div>
-            </form>
-        </div>
+        <Form
+            title="Enter Your Email Address"
+            fieldName="email"
+            fieldLabel="Email Address"
+            fieldType="email"
+            nextPath={PATHS.PHONE}
+        />
     );
 };
 
