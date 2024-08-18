038594d55120d45156b889d17d24e144d6baf844
erinbedri <erinbedri@gmail.com>
2024-08-18 18:51:47 +0200

    Center title in container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ title, titleSize, children }) => {
     return (
-        <div className="flex flex-col gap-10 items-center justify-center h-screen bg-gray-100 p-6">
+        <div className="flex flex-col text-center gap-10 items-center justify-center h-screen bg-gray-100 p-6">
             <h2 className={`${titleSize} font-extrabold`}>{title}</h2>
 
             {children}
